8f6d19659267ed7dd50873ce9604394f49c057db
gsingh <gds_52@yahoo.com>
2026-01-13 10:55:04 -0500

    feat(17-custom-actions): add logic to create PR

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -2,17 +2,40 @@ const core = require('@actions/core')   /* to get inputs */
 const exec = require('@actions/exec')   /* to run command line scripts/commands */
 const github = require('@actions/github');
 const { Octokit } = require('@octokit/core');
+const { log } = require('node:console');
+const { setUncaughtExceptionCaptureCallback } = require('node:process');
 
+const setupGit = async () => {
+    await exec.exec(`git config --global user.name "gh-automation"`);
+    await exec.exec(`git config --global user.email "gh-automation@gmail.com"`);
+};
 
 const validateBranchName = ({branchName}) => /^[a-zA-Z0-9_\-\.\/]+$/.test(branchName); /*RegEx.test, if matches then return tru else false */
 const validateDirectoryName = ({dirName}) => /^[a-zA-Z0-9_\-\/]+$/.test(dirName);
 
+const setupLogger = ({debug, prefix} = { debug: false, prefix: ''}) => ({
+    debug: (message) =>  {
+      if (debug) {
+        core.info(`DEBUG ${prefix}${prefix ? ' : ' : ''}${message}`);
+        // extend the logging functionality 
+      }
+    },
+    info: (message) => {
+        core.info(`${prefix}${prefix ? ' : ' : ''}${message}`);
+    },
+    error: (message) => {
+        core.error(`${prefix}${prefix ? ' : ' : ''}${message}`); /*prefix if no prefix, then add empty or if there is then add :*/
+    }
+});
+
 async function run() {
   const baseBranch = core.getInput('base-branch', { required: true});
-  const targetBranch = core.getInput('target-branch', { required: true});
+  /*const headBranch = core.getInput('head-branch', { required: true});*/
+  const headBranch = core.getInput('head-branch', { required: true});
   const ghToken = core.getInput('gh-token', { required: true});
   const workingDir = core.getInput('working-directory', { required: true}); 
   const debug = core.getBooleanInput('debug');
+  const logger = setupLogger({debug, prefix: '[js-dependency-update]'});
 
   const commonExecOpts = {
     cwd: workingDir
@@ -20,13 +43,15 @@ async function run() {
 
   core.setSecret(ghToken);
 
+  logger.debug('Validating inputs - base-branch, head-branch, working-directory');
+
   if (!validateBranchName({branchName: baseBranch})){
     core.setFailed('Invalid base-branch name. Branch names should include only char, no, hypen, underscore, dots and forward slash');
     return;
   }
 
-  if (!validateBranchName({branchName: targetBranch})){
-    core.setFailed('Invalid target-branch name. Branch names should include only char, no, hypen, underscore, dots and forward slash');
+  if (!validateBranchName({branchName: headBranch})){
+    core.setFailed('Invalid head-branch name. Branch names should include only char, no, hypen, underscore, dots and forward slash');
     return;
   }
 
@@ -35,10 +60,11 @@ async function run() {
     return;
   }
 
-  core.info(`[js-dependency-update] : base branch is ${baseBranch}`);
-  core.info(`[js-dependency-update] : target branch is ${targetBranch}`);
-  core.info(`[js-dependency-update] : working directory is ${workingDir}`);
+  logger.debug(`Base branch is ${baseBranch}`);
+  logger.debug(`Head branch is ${headBranch}`);
+  logger.debug(`Working directory is ${workingDir}`);
 
+  logger.debug('Checking for package updates')
   await exec.exec('npm update', [], {
     ...commonExecOpts
   } );
@@ -48,10 +74,12 @@ async function run() {
   });
 
   if (gitStatus.stdout.length > 0){
-    core.info('[js-dependency-update] : There are updates available!');
-    await exec.exec(`git config --global user.name "gh-automation"`);
-    await exec.exec(`git config --global user.email "gh-automation@gmail.com"`);
-    await exec.exec(`git checkout -b ${targetBranch}`, [], {
+    logger.debug('There are updates available!');
+    logger.debug('Setting up git');
+    await setupGit();
+
+    logger.debug('Commiting and pushing package*.json changes');
+    await exec.exec(`git checkout -b ${headBranch}`, [], {
         ...commonExecOpts,
     });
     await exec.exec(`git add package.json package-lock.json`, [], {
@@ -60,38 +88,42 @@ async function run() {
     await exec.exec(`git commit -m  "chore: update dependencies`, [], {
         ...commonExecOpts,
     });
-    await exec.exec(`git push -u origin ${targetBranch} --force`, [], {
+    await exec.exec(`git push -u origin ${headBranch} --force`, [], {
         ...commonExecOpts,
     });
 
+    logger.debug('Fetching octokit API');
     const octokit = github.getOctokit(ghToken);    /*pass the github token here */
     try {
+      logger.debug(`Creating PR using head branch ${headBranch}`);
       await octokit.rest.pulls.create({
         owner: github.context.repo.owner,
         repo: github.context.repo.repo,
         title: `Update NPM dependencies`,
         body: `This pull request updates NPM packages`,
         base: baseBranch,
-        head: targetBranch
+        head: headBranch
       });
     } catch (e) {
-        core.error('[js-dependency-update]: something went wrong while creating the PR, Check logs below.')
+        logger.error(
+            'Something went wrong while creating the PR, Check logs below.'
+        );
         core.setFailed(e.message);
-        core.error(e);
+        logger.error(e);
     }
   } else {
-    core.info('[js-dependency-update] : No updates at this poing in time!');
+    logger.info('No updates at this poing in time.');
   }
     /*
     1.Parse inputs:
       1.1 Base-branch from which to check for updates
-      1.2 target-branch to use to create the PR
+      1.2 head-branch to use to create the PR
       1.3 Github token for authentication purposes to create PRs
       1.4 working directory for which to check for dependencies
     2. Execute npm update command within the working directory
     3. check whether there are modified pacakge*.json files 
     4 If there are modified files:
-      4.1 Add and commit files to the target-branch
+      4.1 Add and commit files to the head-branch
       4.2 Create a PR to the base-branch using the octokit API (github  API)
     5 Otherwise, conclude the custom action
     
